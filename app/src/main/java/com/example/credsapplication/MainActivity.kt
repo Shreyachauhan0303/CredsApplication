f66f3fcd475e39a84c3442e42d10ba99089e223a
Shreya Chauhan <94741420+Shreyachauhan0303@users.noreply.github.com>
2023-10-28 21:34:27 +0530

    Commit

--- app/src/main/java/com/example/credsapplication/MainActivity.kt
+++ app/src/main/java/com/example/credsapplication/MainActivity.kt
@@ -15,32 +15,6 @@ import com.example.credsapplication.ui.theme.CredsApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CredsApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CredsApplicationTheme {
-        Greeting("Android")
     }
 }
